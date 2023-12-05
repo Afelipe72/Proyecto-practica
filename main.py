effbddbf41b60b55f888c8f9ac1a88123039fa3a
Psoftware <escovarandres8@gmail.com>
2023-12-05 17:25:49 -0500

    05/12/2023

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ previous_frame_bounding_box = {}
 previous_speeds_to_acceleration = {}
 current_frame = 0
 time_elapsed = 0
+time_elapsed_reset = 0
 vx, vy, vt = 0, 0, 0
 ax, ay, at = 0, 0, 0
 format_time_elapsed = ""
@@ -31,6 +32,15 @@ frames_per_second = 0.0333
 
 accumulated_data = []
 zone_timer = 0
+
+user_input_minutes_raw_csv = 0.1  # Input in minutes
+target_second_csv_raw = user_input_minutes_raw_csv * 60
+
+user_input_minutes_zone_timer = 0.1
+target_second_zone = user_input_minutes_zone_timer * 60
+processed_objects = {}
+
+
 def get_center_bounding_box(coordinates_in_detection) -> list:
     x_center = (coordinates_in_detection[0] + coordinates_in_detection[2]) / 2
     y_center = (coordinates_in_detection[1] + coordinates_in_detection[3]) / 2
@@ -44,7 +54,6 @@ def calculate_speed(previous_frame_coordinates, current_frame_coordinates) -> di
         if key in previous_frame_coordinates:
             prev_x, prev_y = previous_frame_coordinates[key]
             current_x, current_y = current_frame_coordinates[key]
-
             x_change = (current_x - prev_x) * GSD / frames_per_second
             y_change = (current_y - prev_y) * GSD / frames_per_second
             speed_vehicle = math.sqrt(x_change ** 2 + y_change ** 2)
@@ -56,18 +65,14 @@ def calculate_speed(previous_frame_coordinates, current_frame_coordinates) -> di
 
 def calculate_acceleration(speed_x_y_total_previous_frame, speed_x_y_total_current_frame) -> dict:
     acceleration = {}
-    for key in speed_x_y_total_previous_frame:
-        if key in speed_x_y_total_current_frame:
+    for key in speed_x_y_total_current_frame:
+        if key in speed_x_y_total_previous_frame:
             x_value_previous, y_value_previous, total_val_previous = speed_x_y_total_previous_frame[key]
             x_value_current, y_value_current, total_val_current = speed_x_y_total_current_frame[key]
-            x_change = (x_value_current - x_value_previous) / frames_per_second
-            y_change = (y_value_current - y_value_previous) / frames_per_second
+            x_change = ((x_value_current - x_value_previous) / frames_per_second)*0.1
+            y_change = ((y_value_current - y_value_previous) / frames_per_second)*0.1
             acceleration_vehicle = math.sqrt(x_change ** 2 + y_change ** 2)
-            acceleration[key] = (
-                f"{x_change:.9f}",
-                f"{y_change:.9f}",
-                f"{acceleration_vehicle:.9f}"
-            )
+            acceleration[key] = (x_change, y_change, acceleration_vehicle)
         else:
             acceleration[key] = (0, 0, 0)
     return acceleration
@@ -171,7 +176,6 @@ def polygon_zone() -> dict:
 zones_dict = polygon_zone()
 zone_vehicle_count = {zone_name: 0 for zone_name in zones_dict.keys()}
 
-processed_objects = {}
 def process_polygon_zone(zones_dict, detections, frame):
     global processed_objects
     for zone_name, zone_data in zones_dict.items():
@@ -213,15 +217,22 @@ def process_polygon_zone(zones_dict, detections, frame):
 
             frame = zone_annotator.annotate(scene=frame)
 
-    # print(f"{processed_objects}")
     return processed_objects
 
 
 def write_csv_polygon_zone(accumulated_data):
-    global header_written_zone, zone_timer
+    global header_written_zone
     car_values_list = []
 
-    # "('test2'[{'vehicle_count': 4, 'tracker_id': 21, 'class_id': 2, 'class_names': 'carros', 'confidence': 69}])"
+    if not header_written_zone:
+        # minuto , topologia , tipo carro, nombre de la zona, conteo
+        vehicles_csv_header = ['Zona', 'Minuto', 'Conteo general', 'Tipología del vehiculo']
+        with open('csv_routes.csv', 'a', newline='') as polygon_zone_csv:
+            writer = csv.writer(polygon_zone_csv)
+            writer.writerow(vehicles_csv_header)
+
+        header_written_zone = True
+
     for key, car_values in accumulated_data.items():
         for value in car_values:
             # items_accumulated_data = {'zone':key, **value}
@@ -233,65 +244,68 @@ def write_csv_polygon_zone(accumulated_data):
             confidence = value['confidence']
 
             # Append the extracted values to the list
-            car_values_list.append(zone_name)
-            car_values_list.append(vehicle_count)
-            car_values_list.append(tracker_id)
-            car_values_list.append(class_id)
-            car_values_list.append(class_names)
-            car_values_list.append(confidence)
-            car_values_list.append(format_time_elapsed)
+            car_values_list.extend([zone_name, format_time_elapsed, vehicle_count, class_names])
+    #SAP
+    # for i in range(accumulated_data):
+    #     if accumulated_data == 1:
+            # Open the CSV file in append mode and write the values
+            with open('csv_routes.csv', 'a', newline='') as polygon_zone_csv:
+                writer = csv.writer(polygon_zone_csv)
+                writer.writerow(car_values_list)
 
-    if not header_written_zone:
-        # minuto , topologia , tipo carro, nombre de la zona, conteo
-        vehicles_csv_header = ['Minuto', 'Zona', 'Conteo general', 'Tipología del vehiculo']
-        with open('csv_routes.csv', 'a', newline='') as polygon_zone_csv:
-            writer = csv.writer(polygon_zone_csv)
-            writer.writerow(vehicles_csv_header)
+            # Clear the list for the next row
+            car_values_list = []
 
-        header_written_zone = True
-
-    # Open the CSV file in append mode and write the values
-    with open('csv_routes.csv', 'a', newline='') as polygon_zone_csv:
-        writer = csv.writer(polygon_zone_csv)
-        writer.writerow(car_values_list)
-
-# line_zone
-# test = sv.LineZone(start=Point(2029, 704), end=Point(2262, 620))
-# line_annotator = sv.LineZoneAnnotator(thickness=2, text_thickness=1, text_scale=1)
-# test.trigger(detections=detections)
-# line_annotator.annotate(frame=frame, line_counter=test)
+def write_report():
+    print("do to")
 
 def callback(frame: np.ndarray, _: int) -> np.ndarray:
+    # Process frame
     results = model(frame)[0]
     detections = sv.Detections.from_ultralytics(results)
     detections = tracker.update_with_detections(detections)
-
-    global current_frame, time_elapsed, format_time_elapsed, CLASS_NAMES_DICT, zones_dict, zone_timer, accumulated_data,\
-        format_time_elapsed_test, processed_objects
+    # Set variables
+    global current_frame, time_elapsed, format_time_elapsed, CLASS_NAMES_DICT, zones_dict, zone_timer, accumulated_data, \
+        format_time_elapsed_test, processed_objects, time_elapsed_reset
     current_frame += 1
+
+    time_elapsed_reset += 1 / 30
+    format_time_elapsed_reset = f"{time_elapsed_reset: 0.3f}"
+    format_time_elapsed_reset_to_float = float(format_time_elapsed_reset)
+
     time_elapsed += 1 / 30
     format_time_elapsed = f"{time_elapsed: 0.3f}"
 
+
+    # Process for the raw csv values
     for car_value in range(len(detections)):
         values_to_csv = format_csv_values(detections.tracker_id[car_value], detections.xyxy[car_value],
                                           CLASS_NAMES_DICT.get(detections.class_id[car_value]), detections.confidence[car_value])
-        write_values_on_csv_raw(values_to_csv)
 
+        if format_time_elapsed_reset_to_float == target_second_csv_raw:
+            write_values_on_csv_raw(values_to_csv)
+
+    if format_time_elapsed_reset_to_float == target_second_csv_raw:
+        time_elapsed_reset = 0
+
+    # Process the polygon zone
     process_polygon_zone(zones_dict, detections, frame)
-
+    # Polygon zone timer
     zone_timer += 1 / 30
     format_time_elapsed_test = f"{zone_timer: 0.3f}"
     print(format_time_elapsed_test)
-    if abs(float(format_time_elapsed_test) - 1.0) < 0.001:
+    if abs(float(format_time_elapsed_test) - target_second_zone) < 0.001:
         write_csv_polygon_zone(process_polygon_zone(zones_dict, detections, frame))
         processed_objects = {}
         zone_timer = 0
+
 
     labels = [
         f"#{tracker_id} {results.names[class_id]}"
         for class_id, tracker_id
         in zip(detections.class_id, detections.tracker_id)
     ]
+
     annotated_frame = box_annotator.annotate(
         frame.copy(), detections=detections)
 
